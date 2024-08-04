158a045cc096803f1f67c4a66f1ae84438fce14a
ByteNinja <pijayblog@gmail.com>
2024-08-04 16:08:29 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
+const validateUser = require('../middlewares/validate');
 const router = express.Router();
 
-router.post('/register', register);
+router.post('/register', validateUser, register);
 router.post('/login', login);
 
 module.exports = router;
