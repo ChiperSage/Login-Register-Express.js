fdaaf8bbe859a9fe84d2e794a504090d2c81192e
ByteNinja <pijayblog@gmail.com>
2024-08-04 16:06:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./models');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 require('dotenv').config();
@@ -10,10 +10,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch(err => console.error(err));
+db.sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+}).catch(err => console.error(err));
