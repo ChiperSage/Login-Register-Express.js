907e410a495f6fa27dfa8b6af924fa30ef3a3303
ByteNinja <pijayblog@gmail.com>
2024-08-12 17:08:03 +0700

    Update postgre.sql

--- postgre.sql
+++ postgre.sql
@@ -2,6 +2,7 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     login_attempts INT DEFAULT 0,
     last_login_attempt INT DEFAULT 0,
