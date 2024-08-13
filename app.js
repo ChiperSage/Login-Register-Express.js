b43906c67bcc9a4cb632c62ad6c43b8634cba928
ByteNinja <pijayblog@gmail.com>
2024-08-13 10:49:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
-const db = require('./models');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-require('dotenv').config();
-
 const app = express();
+const path = require('path');
 
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
 
-db.sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
-}).catch(err => console.error(err));
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Import the auth routes
+const authRoutes = require('./routes/auth');
+
+// Use the auth routes
+app.use('/auth', authRoutes);
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
