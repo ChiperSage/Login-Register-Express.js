a22c714731f8d6f63e56bdd9bd1a5e42c9e841a1
ByteNinja <pijayblog@gmail.com>
2024-08-13 17:55:38 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,13 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// **New Code: Set up the MySQL connection**
-const connection = mysql.createConnection({
-    host: 'localhost', // Your database host
-    user: 'root',      // Your database user
-    password: '', // Your database password
-    database: 'express_auth' // Your database name
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'express_auth',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// **New Code: Connect to the database**
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.stack);
-        return;
-    }
-    console.log('Connected to MySQL as id ' + connection.threadId);
-});
-
-module.exports = connection;
+module.exports = db;
